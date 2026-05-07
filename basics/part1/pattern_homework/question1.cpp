bcf5e859f2d4e173ee6d0ad7b205d94f31c32df1
Prasun <prasun.3104@gmail.com>
2026-05-07 19:57:59 +0530

    question1

---
+++ basics/part1/pattern_homework/question1.cpp
@@ -0,0 +1,24 @@
+/*
+1111
+2222
+3333
+4444
+
+format the code by.
+cmd + k
+cmd + f
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n;i++){
+        for(int j = 1 ;j<=n;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
