eec7851b1412804e9a5497f4a8ee937e7406fcb4
Prasun <prasun.3104@gmail.com>
2026-05-07 20:24:03 +0530

    questioin02

---
+++ basics/part1/pattern_homework/question02.cpp
@@ -0,0 +1,19 @@
+/*
+1234
+123
+12
+1
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for (int i = 1; i <= n;i++){
+        for(int j = 1 ;j<=n+1-i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
